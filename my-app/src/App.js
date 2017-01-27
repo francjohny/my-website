cd0d9ab53f36b1f84a1d3ca0029a060237297576
Francis Johny <francjohny@gmail.com>
2017-01-28 05:07:39 +0530

    Redesign

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,30 +5,48 @@ class App extends Component {
   render() {
     return (
 		<div className="App">
+			<nav className="App-header">
+		        <ul className="list">
+			        <a href="#">
+						<li>Home</li>
+			        </a>
+			        <a href="#">
+						<li>Portfolio</li>
+			        </a>
+			        <a href="#">
+						<li>About</li>
+			        </a>
+			        <a href="#">
+						<li>Resume</li>
+			        </a>
+			        <a href="#">
+						<li>Contact</li>
+			        </a>
+		        </ul>
+	    	</nav>
 			<div className="App-body">
-		        <h2 className="h2">Hi!</h2>
-		        <h2 className="h2">Francis!</h2>
+				<strong className="highlight"> developer </strong>
 			</div>
 		    <nav className="App-nav">
 		        <ul className="list">
 			        <a href="https://in.linkedin.com/in/francis-johny">
 						<li>
-			            <i style={{ "color": "#4875B4"}} className="fa fa-linkedin" aria-hidden="true"></i>
+			            	<i style={{ "color": "#fff"}} className="fa fa-linkedin" aria-hidden="true"></i>
 						</li>
 			        </a>
 			        <a href="https://github.com/francjohny">
 						<li>
-			            <i style={{ "color": "#4875B4"}} className="fa fa-github" aria-hidden="true"></i>
+			            	<i style={{ "color": "#fff"}} className="fa fa-github" aria-hidden="true"></i>
 						</li>
 			        </a>
 			        <a href="https://twitter.com/13urstskill">
 						<li>
-			            <i style={{ "color": "#4875B4"}} className="fa fa-twitter" aria-hidden="true"></i>
+			            	<i style={{ "color": "#fff"}} className="fa fa-twitter" aria-hidden="true"></i>
 						</li>
 			        </a>
 			        <a href="mailto:francjohny@gmail.com">
 						<li>
-			            <i style={{ "color": "#4875B4"}} className="fa fa-envelope-o" aria-hidden="true"></i>
+			            	<i style={{ "color": "#fff"}} className="fa fa-envelope-o" aria-hidden="true"></i>
 						</li>
 			        </a>
 		        </ul>
