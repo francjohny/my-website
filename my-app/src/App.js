c98727921473534418b3a435673241221138ffd3
Francis Johny <francjohny@gmail.com>
2017-01-30 01:31:06 +0530

    Modularise into React components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,56 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header/Header.js';
+import Main from './Main/Main.js';
+import Aside from './Aside/Aside.js';
 
 class App extends Component {
   render() {
     return (
 		<div className="App">
-			<nav className="App-header">
-		        <ul className="list">
-			        <a href="#">
-						<li>Home</li>
-			        </a>
-			        <a href="#">
-						<li>Portfolio</li>
-			        </a>
-			        <a href="#">
-						<li>About</li>
-			        </a>
-			        <a href="#">
-						<li>Resume</li>
-			        </a>
-			        <a href="#">
-						<li>Contact</li>
-			        </a>
-		        </ul>
-	    	</nav>
-			<div className="App-body">
-				<strong className="highlight"> developer </strong>
-			</div>
-		    <nav className="App-nav">
-		        <ul className="list">
-			        <a href="https://in.linkedin.com/in/francis-johny">
-						<li>
-			            	<i style={{ "color": "#fff"}} className="fa fa-linkedin" aria-hidden="true"></i>
-						</li>
-			        </a>
-			        <a href="https://github.com/francjohny">
-						<li>
-			            	<i style={{ "color": "#fff"}} className="fa fa-github" aria-hidden="true"></i>
-						</li>
-			        </a>
-			        <a href="https://twitter.com/13urstskill">
-						<li>
-			            	<i style={{ "color": "#fff"}} className="fa fa-twitter" aria-hidden="true"></i>
-						</li>
-			        </a>
-			        <a href="mailto:francjohny@gmail.com">
-						<li>
-			            	<i style={{ "color": "#fff"}} className="fa fa-envelope-o" aria-hidden="true"></i>
-						</li>
-			        </a>
-		        </ul>
-		    </nav>
+			<Header />
+			<Main />
 		</div>
     );
   }
