26176124986d8e39891a4461c6e6dbcfac1fe4ed
Francis Johny <francjohny@gmail.com>
2017-08-27 22:55:58 +0530

    Change display picture.

--- my-app/src/DisplayPicture/DisplayPicture.js
+++ my-app/src/DisplayPicture/DisplayPicture.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import './DisplayPicture.css';
-import me from './me.svg';
+import me from './me-without-glasses.svg';
 
 class DisplayPicture extends Component {
     render() {
         return (
             <div className="display-picture">
-                <img src={me} alt="my pic"/>
+                <img className="my-picture" src={me} alt="my pic"/>
             </div>
         );
     }
