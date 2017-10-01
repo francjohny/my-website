5b6b60e2d4211dc2956d5fc15eb09097690998c1
Francis Johny <francjohny@gmail.com>
2017-10-01 15:54:39 +0530

    Style active header links

--- my-app/src/TransHeader/TransHeader.js
+++ my-app/src/TransHeader/TransHeader.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import "./TransHeader.css";
 import '../Header/Header.css';
 
@@ -10,9 +10,9 @@ class TransHeader extends Component {
                 <nav className="header-nav">
                     <ul className="header-list">
                         <li className="logo"><Link to="/" style={{color: this.props.color}}> Francis Johny </Link></li>
-                        <li className="header-nav-item"><Link to="/portfolio" style={{color: this.props.color}}> Portfolio </Link></li>
-                        <li className="header-nav-item"><Link to="/about" style={{color: this.props.color}}> About </Link></li>
-                        <li className="header-nav-item"><Link to="/contact" style={{color: this.props.color}}> Contact </Link></li>
+                        <li className="header-nav-item"><NavLink to="/portfolio" style={{color: this.props.color}} activeStyle={{opacity: '1'}}> Portfolio </NavLink></li>
+                        <li className="header-nav-item"><NavLink to="/about" style={{color: this.props.color}} activeStyle={{opacity: '1'}}> About </NavLink></li>
+                        <li className="header-nav-item"><NavLink to="/contact" style={{color: this.props.color}} activeStyle={{opacity: '1'}}> Contact </NavLink></li>
                     </ul>
                 </nav>
             </header>
