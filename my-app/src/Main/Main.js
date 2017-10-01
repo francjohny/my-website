1945cf8b01099c72aeb2cb20a60cebfc9cc45579
Francis Johny <francjohny@gmail.com>
2017-10-01 15:59:12 +0530

    Change page title with DocumentTitle.

--- my-app/src/Main/Main.js
+++ my-app/src/Main/Main.js
@@ -8,6 +8,7 @@ import TransHeader from '../TransHeader/TransHeader.js';
 import TransFooter from '../TransFooter/TransFooter.js';
 import Footer from '../Footer/Footer.js';
 import { Switch, Route } from 'react-router-dom';
+import DocumentTitle from 'react-document-title';
 import './Main.css';
 
 class Main extends Component {
@@ -19,13 +20,13 @@ class Main extends Component {
 				<Route exact path="/portfolio" component={PortfolioPage}/>
 				<Route path="/portfolio/es6" component={() => {
 					return TalkPage({
-						"title": "ES6 Presentation",
+						"title": "Exploring ES6 & Beyond",
 						"src": "https://www.icloud.com/keynote/0Vs2kAmcjlUOkXyStYL6TWfHw?embed=true"
 					})
 				}}/>
 				<Route path="/portfolio/blockchain" component={() => {
 					return TalkPage({
-						"title": "Blockchain Presentation",
+						"title": "Blockchain Demystified",
 						"src": "https://www.icloud.com/keynote/0e1UN2MfHz6cnETziv6Sc-Msg?embed=true"
 					})
 				}}/>
@@ -39,49 +40,59 @@ class Main extends Component {
 }
 
 const HomePage = () => (
-	<div>
-		<TransHeader color="#000"/>
-		<DisplayPicture/>
-		<TransFooter backgroundColor="#f8fdff"/>
-	</div>
-)
+	<DocumentTitle title='Francis Johny'>
+		<div>
+			<TransHeader color="#000"/>
+			<DisplayPicture/>
+			<TransFooter backgroundColor="#f8fdff"/>
+		</div>
+	</DocumentTitle>
+);
 
 const PortfolioPage = () => (
-	<div>
-		<TransHeader color="#000"/>
-		<Portfolio/>
-		<TransFooter backgroundColor="#f8fdff"/>
-	</div>
-)
+	<DocumentTitle title='Portfolio'>
+		<div>
+			<TransHeader color="#000"/>
+			<Portfolio/>
+			<TransFooter backgroundColor="#f8fdff"/>
+		</div>
+	</DocumentTitle>
+);
 
 const TalkPage = ({ title, src, width="100%", height="740" }) => {
 	return (
-		<div>
-			<iframe title={title}
-					src={src}
-					width={width}
-					height={height}
-					frameborder="0"
-					allowfullscreen="1"
-					referrer="no-referrer"
-			/>
-			<Footer/>
-		</div>
+		<DocumentTitle title={title}>
+			<div>
+				<iframe title={title}
+						src={src}
+						width={width}
+						height={height}
+						frameBorder="0"
+						allowFullScreen="1"
+						referrer="no-referrer"
+				/>
+				<Footer/>
+			</div>
+		</DocumentTitle>
 	)
-}
+};
 
 const AboutPage = () => (
-	<div>
-		<TransHeader color="#000"/>
-		<About/>
-	</div>
-)
+	<DocumentTitle title='About'>
+		<div>
+			<TransHeader color="#000"/>
+			<About/>
+		</div>
+	</DocumentTitle>
+);
 
 const ContactPage = () => (
-	<div>
-		<TransHeader color="#ddd"/>
-		<Contact color="fff"/>
-	</div>
-)
+	<DocumentTitle title='Contact'>
+		<div>
+			<TransHeader color="#ddd"/>
+			<Contact color="fff"/>
+		</div>
+	</DocumentTitle>
+);
 
 export default Main;
