728ae90e163ba6a40fee75baaf220eb195b4bb50
Francis Johny <francjohny@gmail.com>
2017-12-10 12:57:03 +0530

    Make display picture redirect to about page.

--- my-app/src/DisplayPicture/DisplayPicture.js
+++ my-app/src/DisplayPicture/DisplayPicture.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import './DisplayPicture.css';
+import { NavLink } from 'react-router-dom';
 import me from './me.svg';
 
 class DisplayPicture extends Component {
     render() {
         return (
             <div className="display-picture">
-                <img className="my-picture" src={me} alt="my pic"/>
+                <NavLink to="/about"><img className="my-picture" src={me} alt="my pic"/></NavLink>
             </div>
         );
     }
