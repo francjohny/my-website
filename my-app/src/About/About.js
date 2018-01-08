df4695aa3bf17233a26090109dc071baacc680f3
Francis Johny <francjohny@gmail.com>
2018-01-09 01:08:29 +0530

    Change logo

--- my-app/src/About/About.js
+++ my-app/src/About/About.js
@@ -2,42 +2,33 @@ import React, {Component} from 'react';
 import './About.css';
 import { Link } from 'react-router-dom';
 import TransFooter from '../TransFooter/TransFooter.js';
-import { CSSTransitionGroup } from 'react-transition-group';
 
 class About extends Component {
     render() {
         return (
-            <CSSTransitionGroup
-                style={{width:"100%"}}
-                transitionName="slideOut"
-                transitionAppear={true}
-                transitionAppearTimeout={250}
-                transitionEnter={false}
-                transitionLeave={false}>
-                <div className="about">
-                    <section className="title">
-                        Hello World
-                    </section>
-                    <section className="about-intro">
-                        <p>
-                            I'm Francis Johny! I’m a programmer and a maker of things.
-                            In my spare time, I build, explore and teach at Khan Academy.
-                            I'm currently a Software Developer at
-                            <a style={{color: "#2200CC", marginRight: "0", textDecoration: "none"}}
-                               href="https://www.qburst.com/">
-                                &nbsp;QBurst&nbsp;
-                            </a>.
-                            If you're reading this, we should be friends!
-                            Let's build something amazing together.
-                            <Link style={{color: "#2200CC", marginRight: "0", textDecoration: "none"}}
-                                  to="/contact">
-                                &nbsp;Say Hi!&nbsp;
-                            </Link>
-                        </p>
-                    </section>
-                    <TransFooter backgroundColor="#fff"/>
-                </div>
-            </CSSTransitionGroup>
+            <div className="about">
+                <section className="title">
+                    Hello World
+                </section>
+                <section className="about-intro">
+                    <p>
+                        I'm Francis Johny! I’m a programmer and a maker of things.
+                        In my spare time, I build, explore and teach at Khan Academy.
+                        I'm currently a Software Developer at
+                        <a style={{color: "#2200CC", marginRight: "0", textDecoration: "none"}}
+                           href="https://www.qburst.com/">
+                            &nbsp;QBurst&nbsp;
+                        </a>.
+                        If you're reading this, we should be friends!
+                        Let's build something amazing together.
+                        <Link style={{color: "#2200CC", marginRight: "0", textDecoration: "none"}}
+                              to="/contact">
+                            &nbsp;Say Hi!&nbsp;
+                        </Link>
+                    </p>
+                </section>
+                <TransFooter backgroundColor="#fff"/>
+            </div>
         );
     }
 }
--- my-app/src/DisplayPicture/DisplayPicture.js
+++ my-app/src/DisplayPicture/DisplayPicture.js
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
 import "./DisplayPicture.css";
 import me from "./me.svg";
+import francis from "./francis.png";
 
 class DisplayPicture extends Component {
     render() {
         return (
             <div className="display-picture">
-                <div className="logo">Francis Johny</div>
+                <img className="my-logo" src={francis} alt="Francis Johny"/>
                 <img className="my-picture" src={me} alt="Francis Johny"/>
             </div>
         );
