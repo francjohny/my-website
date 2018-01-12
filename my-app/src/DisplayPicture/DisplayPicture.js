39e3aeed836e14f869a63df0c71c28fe29552104
Francis Johny <francjohny@gmail.com>
2018-01-12 22:20:03 +0530

    Redesign: v4

--- my-app/src/DisplayPicture/DisplayPicture.js
+++ my-app/src/DisplayPicture/DisplayPicture.js
@@ -1,13 +1,12 @@
 import React, {Component} from "react";
 import "./DisplayPicture.css";
 import me from "./me.svg";
-import francis from "./francis.png";
 
 class DisplayPicture extends Component {
     render() {
         return (
             <div className="display-picture">
-                <img className="my-logo" src={francis} alt="Francis Johny"/>
+                <h1 className="my-logo">FRANCIS JOHNY</h1>
                 <img className="my-picture" src={me} alt="Francis Johny"/>
             </div>
         );
