60bff57c9dd4910847e4abf535d59a429472fed6
Francis Johny <francjohny@gmail.com>
2018-05-06 15:12:45 +0530

    Add initial animation

--- my-app/src/DisplayPicture/DisplayPicture.js
+++ my-app/src/DisplayPicture/DisplayPicture.js
@@ -1,17 +1,55 @@
 import React, {Component} from "react";
 import "./DisplayPicture.css";
 import me from './me-without-glasses.svg';
-import { NavLink } from 'react-router-dom';
+import hoverMe from './me.svg';
+import {NavLink} from 'react-router-dom';
+import {CSSTransitionGroup} from 'react-transition-group';
 
 class DisplayPicture extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            imgSrc: me
+        };
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
+    }
+
+    handleMouseOver() {
+        this.setState({
+            imgSrc: hoverMe
+        });
+    }
+
+    handleMouseOut() {
+        this.setState({
+            imgSrc: me
+        });
+    }
 
     render() {
         return (
             <div className="display-picture">
+                <CSSTransitionGroup
+                    transitionName="hi-animate"
+                    transitionAppear={true}
+                    transistionAppearTimeout={150}
+                    transitionEnter={false}
+                    transitionLeave={false}>
+                    <section className="hi"> Hi! </section>
+                </CSSTransitionGroup>
+                <CSSTransitionGroup
+                    transitionName="slideOut"
+                    transitionAppear={true}
+                    transistionAppearTimeout={200}
+                    transitionEnter={false}
+                    transitionLeave={false}>
+                    <section className="hi"> Hi! </section>
                 <NavLink to="/about">
-                    <img src={me} alt="me" className="my-picture"/>
+                    <img src={this.state.imgSrc} alt="me" className="my-picture" onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}/>
                 </NavLink>
-
+                </CSSTransitionGroup>
             </div>
         );
     }
